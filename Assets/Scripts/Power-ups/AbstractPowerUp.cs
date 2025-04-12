1ab27ab9608ec95294664638b9f007a215dcb1c5
Juliusz Trochimczuk <assasindal@gmail.com>
2025-04-12 10:58:53 +0200

    abstact

--- Assets/Scripts/Power-ups/AbstractPowerUp.cs
+++ Assets/Scripts/Power-ups/AbstractPowerUp.cs
@@ -1,18 +1,32 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class AbstractPowerUp : MonoBehaviour
 {
     private List<GameObject> players;
-    
-    public void Start()
-    {
-        players =  
-    }
+    [SerializeField]
+    private float rangeDistance;
 
     public void Awake()
     {
-        
+        players = GameObject.FindGameObjectsWithTag("Player").ToList();
     }
+
+    public void FixedUpdate()
+    {
+        foreach (var player in players)
+        {
+            if (player == null)
+                players.Remove(player);
+
+            var distance = Vector3.Distance(player.transform.position, player.transform.forward);
+            if (distance < rangeDistance)
+            {
+                Activate();
+            }
+        }
+    }
+
+    public abstract void Activate();
 }
---
+++ Assets/Scripts/Power-ups/Storage.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Storage : MonoBehaviour
+{
+    private AbstractPowerUp _currentPowerUp;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
