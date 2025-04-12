a5fc547e580674249965b4bc52d789f63c97da10
invi <roflankaa@gmail.com>
2025-04-12 10:35:03 +0200

    xx

---
+++ Assets/Scripts/Power-ups/AbstractPowerUp.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractPowerUp : MonoBehaviour
+{
+    private List<GameObject> players;
+    
+    public void Start()
+    {
+        players =  
+    }
+
+    public void Awake()
+    {
+        
+    }
+}
