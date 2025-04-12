d861c48687df798d2212936425c3e520909ab6ce
Juliusz Trochimczuk <assasindal@gmail.com>
2025-04-12 21:16:22 +0200

    Camera works

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -1,12 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Controllers
 {
     public class GameController : MonoBehaviour
     {
         private InputSystem_Actions input;
+        private List<PlayerInput> players = new();
 
         public static GameController Instance { get; private set; }
+
+        [SerializeField] private LayerMask[] playerLayerMasks;
+        [SerializeField] private LayerMask[] playersCullingMask;
 
         private void Awake()
         {
@@ -26,6 +32,18 @@ namespace Controllers
         private void OnDisable()
         {
             input.Disable();
+        }
+
+        public void AddPlayer(PlayerInput player)
+        {
+            players.Add(player);
+
+            Transform playerParent = player.transform.root;
+
+            int layerToAdd = (int)Mathf.Log(playerLayerMasks[players.Count - 1].value, 2);
+            playerParent.GetComponentInChildren<Cinemachine.CinemachineVirtualCamera>().gameObject.layer = layerToAdd;
+            playerParent.GetComponentInChildren<Camera>().cullingMask = playersCullingMask[players.Count - 1].value;
+
         }
     }
 }
