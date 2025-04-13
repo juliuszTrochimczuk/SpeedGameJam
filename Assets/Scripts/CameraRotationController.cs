fa9a4910e08f441eae44cfcbd0325956f0a079f9
Mikołaj Falkowski <116456197+M-T-F@users.noreply.github.com>
2025-04-13 10:41:08 +0200

    camera rotation fix

--- Assets/Scripts/CameraRotationController.cs
+++ Assets/Scripts/CameraRotationController.cs
@@ -11,7 +11,6 @@ public class CameraRotationController : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private Transform playerTransform;
     [SerializeField] private InputActionReference rotationX;
-    [SerializeField] private InputActionReference rotationSwitch;
     [SerializeField] private int sensitivity;
     [SerializeField] private int backRotationSpeed;
     private float _rotationX;
@@ -20,32 +19,19 @@ public class CameraRotationController : MonoBehaviour
     
     private void Awake() {
         rotationX.action.performed += OnRotate;
-        rotationSwitch.action.performed += OnEnableRotation;
-        rotationSwitch.action.canceled += OnDisableRotation;
     }
     
     private void OnRotate(InputAction.CallbackContext context) {
-        if (_rotating) {
-            _rotationX = context.ReadValue<float>() * sensitivity;
-        }
+        _rotationX = context.ReadValue<float>() * sensitivity;
     }
-
-    private void OnEnableRotation(InputAction.CallbackContext context)
-    {
-        _rotating = true;
-    }
-
-    private void OnDisableRotation(InputAction.CallbackContext context)
-    {
-        _rotating = false;
-    }
+    
 
     void Update() {
-        if (_rotating) {
-            target.Rotate(Vector3.up, _rotationX * Time.deltaTime);
-        }else{
-            target.Rotate(Vector3.up, (playerTransform.forward.x-target.forward.x)*backRotationSpeed * Time.deltaTime);
-        }
+        target.Rotate(Vector3.up, _rotationX * Time.deltaTime);
+        target.rotation = Quaternion.Euler(target.rotation.eulerAngles.x, target.rotation.eulerAngles.y, 0);
+        
+        
+        
         
     }
 }
